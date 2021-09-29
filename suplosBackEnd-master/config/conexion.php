290d839a53ca754da237c2ea26d4d566a1575c3d
SheilaMadeleni <67665756+SheilaMadeleni@users.noreply.github.com>
2021-09-29 03:31:47 -0500

    Add files via upload

---
+++ suplosBackEnd-master/config/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "123", "intelcost_bienes");
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+
+
